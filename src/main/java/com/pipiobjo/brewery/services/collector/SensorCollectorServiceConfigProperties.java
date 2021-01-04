30e4dc17c5f54308bbab6c00de4b2031bc00c40a
Björn <pipiobjo@googlemail.com>
2021-01-04 02:36:19 +0100

    add config validators

--- src/main/java/com/pipiobjo/brewery/services/collector/SensorCollectorServiceConfigProperties.java
+++ src/main/java/com/pipiobjo/brewery/services/collector/SensorCollectorServiceConfigProperties.java
@@ -3,14 +3,18 @@ package com.pipiobjo.brewery.services.collector;
 import io.quarkus.arc.config.ConfigProperties;
 import lombok.Data;
 
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 @Data
 @ConfigProperties(prefix = "brewery.sensors")
 public class SensorCollectorServiceConfigProperties {
-    private long baseCollectionIntervallInMS;
-    private long inputCollectionIntervallInMS;
-    private long temperatureCollectionIntervallInMS;
-    private long persistenceIntervallInMS;
-    private long uiUpdateIntervallInMS;
-    private long calculationIntervallInMS;
+
+    @Positive() private long baseCollectionIntervallInMS;
+    @Positive() private long inputCollectionIntervallInMS;
+    @Positive() private long temperatureCollectionIntervallInMS;
+    @Positive() private long persistenceIntervallInMS;
+    @Positive() private long uiUpdateIntervallInMS;
+    @Positive() private long calculationIntervallInMS;
 
 }
