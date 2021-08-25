4574206145a8b1a55e0a23d25a259011713f7895
simpe00 <simpe00>
2021-08-26 01:40:51 +0200

    bugfix Pt1Sys & rename actions "arm" job

--- src/main/java/com/pipiobjo/brewery/services/simulation/Pt1Sys.java
+++ src/main/java/com/pipiobjo/brewery/services/simulation/Pt1Sys.java
@@ -19,8 +19,8 @@ public class Pt1Sys {
     public BigDecimal calculate(BigDecimal stepSizeBD, BigDecimal input){
         BigDecimal xdot;
         // PT1 with forward euler
-        xdot = kGain.multiply(input).subtract(x).divide(timeConstant, 2, RoundingMode.HALF_DOWN);
-        x = x.add(stepSizeBD.multiply(xdot).divide(BigDecimal.valueOf(1000), 2, RoundingMode.HALF_DOWN));
+        xdot = kGain.multiply(input).subtract(x).divide(timeConstant, 15, RoundingMode.HALF_DOWN);
+        x = x.add(stepSizeBD.multiply(xdot).divide(BigDecimal.valueOf(1000), 15, RoundingMode.HALF_DOWN));
         return x;
     }
 }
